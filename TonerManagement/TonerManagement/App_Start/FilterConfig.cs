5d74a6abd0fb78f4fd34c5a71ea6cddacaf58a71
Charles Hardy <charlie.hardy@outlook.com>
2019-01-23 15:00:46 +0000

    Initial Project
    Initial Project

---
+++ TonerManagement/TonerManagement/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TonerManagement
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
