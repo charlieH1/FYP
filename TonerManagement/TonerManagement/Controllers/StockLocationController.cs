24a8430dedb4c9c45fcb0e24d5f95251045324e6
Charles Hardy <charlie.hardy@outlook.com>
2019-03-25 19:20:20 +0000

    CSHTML written
    Begining of CSHTML written. Rest of javascript to populate and complete tasks needs to be written

---
+++ TonerManagement/TonerManagement/Controllers/StockLocationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TonerManagement.Handlers.Interface;
+
+namespace TonerManagement.Controllers
+{
+    public class StockLocationController : Controller
+    {
+        private readonly IUserHandler _userHandler;
+        public StockLocationController(IUserHandler userHandler)
+        {
+            _userHandler = userHandler;
+        }
+        // GET: StockLocation
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null || _userHandler.GetUsers((string)Session["UserName"]).Count == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+            return View("Index");
+        }
+
+
+    }
+}
---
+++ TonerManagement/TonerManagementTests/Controllers/StockLocationControllerTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TonerManagement.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FluentAssertions;
+using Moq;
+using TonerManagement.Handlers.Interface;
+using TonerManagement.Models;
+using TonerManagementTests.TestHelpers;
+
+namespace TonerManagement.Controllers.Tests
+{
+    [TestClass()]
+    public class StockLocationControllerTests
+    {
+        [TestMethod()]
+        public void IndexRequestUserLoggedInReturnsIndexPage()
+        {
+            //setup
+            const string userName = "Test";
+            var mockSession = new MockSessionStateBase { ["UserName"] = userName };
+            var mockUserHandler = new Mock<IUserHandler>();
+           
+            var mockContext = new Mock<ControllerContext>();
+            mockUserHandler.Setup(mUH => mUH.GetUsers(userName)).Returns(new List<User>
+                {new User() {hashedPassword = "junk", userId = 1, userLogin = userName}});
+
+            mockContext.Setup(mC => mC.HttpContext.Session).Returns(mockSession);
+
+            var sut = new StockLocationController(mockUserHandler.Object)
+            {
+                ControllerContext = mockContext.Object
+            };
+            //action
+            var res = (ViewResult)sut.Index();
+
+            //assert
+            res.ViewName.Should().Be("Index");
+        }
+
+        [TestMethod()]
+        public void IndexRequestNoUserLoggedInReturnsHttpStatusCodeUnauthorized()
+        {
+            //setup
+            const string userName = "Test";
+            var mockSession = new MockSessionStateBase { ["UserName"] = null };
+            var mockUserHandler = new Mock<IUserHandler>();
+
+            var mockContext = new Mock<ControllerContext>();
+            mockUserHandler.Setup(mUH => mUH.GetUsers(userName)).Returns(new List<User>
+                {new User() {hashedPassword = "junk", userId = 1, userLogin = userName}});
+
+            mockContext.Setup(mC => mC.HttpContext.Session).Returns(mockSession);
+
+            var sut = new StockLocationController(mockUserHandler.Object)
+            {
+                ControllerContext = mockContext.Object
+            };
+            //action
+            var res = (HttpStatusCodeResult)sut.Index();
+
+            //assert
+            res.Should().BeEquivalentTo(new HttpStatusCodeResult(HttpStatusCode.Unauthorized));
+        }
+    }
+}
