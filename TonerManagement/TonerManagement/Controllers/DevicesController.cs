996a4fe15832c36ccb49fac8af92d87da3a34c6e
Charles Hardy <charlie.hardy@outlook.com>
2019-04-01 22:01:46 +0100

    Device CSHTML created
    Devices page fully coded

---
+++ TonerManagement/TonerManagement/Controllers/DevicesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TonerManagement.Controllers
+{
+    public class DevicesController : Controller
+    {
+        // GET: Devices
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetDeviceDetails()
+        {
+            //ToDo implement fetching details in such a way where props are success customer and printer
+            throw new NotImplementedException();
+        }
+
+        public ActionResult GetTonerPercentage()
+        {
+            //ToDo implement fetching current toner percentage
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TonerManagement/TonerManagement/Models/PrinterResponseModelWithAverageTonerCoverage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TonerManagement.Models
+{
+    public class PrinterResponseModelWithAverageTonerCoverage
+    {
+        public int PrinterId { get; set; }
+        public string PrinterName { get; set; }
+        public double AverageTonerCoverage { get; set; }
+        public int CyanLowTonerPercentage { get; set; }
+        public int YellowLowTonerPercentage { get; set; }
+        public int MagentaLowTonerPercentage { get; set; }
+        public int KeyingLowTonerPercentage { get; set; }
+
+    }
+}
