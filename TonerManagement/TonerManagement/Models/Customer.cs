1762e755bd668f41788cde88ce67e7a0f1c94c9a
Charles Hardy <charlie.hardy@outlook.com>
2019-04-17 21:20:55 +0100

    Customer Circular dependancy

--- TonerManagement/TonerManagement/Models/Customer.cs
+++ TonerManagement/TonerManagement/Models/Customer.cs
@@ -14,24 +14,9 @@ namespace TonerManagement.Models
     
     public partial class Customer
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Customer()
-        {
-            this.Printers = new HashSet<Printer>();
-            this.StockLocations = new HashSet<StockLocation>();
-            this.UserCustomers = new HashSet<UserCustomer>();
-        }
-    
         public int customerID { get; set; }
         public string customerName { get; set; }
         public string customerAddress { get; set; }
         public string customerContactNumber { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Printer> Printers { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<StockLocation> StockLocations { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<UserCustomer> UserCustomers { get; set; }
     }
 }
